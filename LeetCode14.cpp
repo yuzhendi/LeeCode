088da84594b17325049af79d60ea528ac694ade0
yuzhendi <1316215578@qq.com>
2020-03-03 18:15:33 +0800

    最长公共前缀

---
+++ LeetCode14.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: LeetCode14.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年03月03日 星期二 18时15分10秒
+ ************************************************************************/
+char * longestCommonPrefix(char ** strs, int strsSize){
+    if(strsSize > 0)
+	{
+		char *ans = (char*)calloc(strlen(strs[0])+1, sizeof(char));
+		char p;
+		int i, j = 0, k = 0;
+		for(i = 0;strs[i][j]!='\0';++i)
+		{
+		        if(i == 0) p = strs[i][j];
+			if(strs[i][j]!=p) break;
+			if(i == strsSize - 1)
+			{
+				i = -1;
+				++j;
+				ans[k++] = p;
+			}
+		}
+		return ans;
+	}
+	else return "";
+}
